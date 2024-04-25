796d4eb86a63579859df7e3960f004fb0e77c095
jPraiser23 <praiserjohn@gmail.com>
2024-04-24 20:17:10 -0400

    Initial commit

---
+++ RecallPrototype/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace RecallPrototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browseButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.selectLocationButton = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(644, 65);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(92, 42);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.selectFileButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(552, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(552, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // selectLocationButton
+            // 
+            this.selectLocationButton.Location = new System.Drawing.Point(644, 124);
+            this.selectLocationButton.Name = "selectLocationButton";
+            this.selectLocationButton.Size = new System.Drawing.Size(92, 46);
+            this.selectLocationButton.TabIndex = 4;
+            this.selectLocationButton.Text = "Select Location";
+            this.selectLocationButton.UseVisualStyleBackColor = true;
+            this.selectLocationButton.Click += new System.EventHandler(this.selectFolderLocation);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(70, 180);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(666, 44);
+            this.convertButton.TabIndex = 5;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(70, 152);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(552, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Path";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Select Save Location";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(67, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Select File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.selectLocationButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.browseButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button selectLocationButton;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ RecallPrototype/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using JDP;
+namespace RecallPrototype
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void selectFileButton_Click(object sender, EventArgs e)
+        {
+            //Get File
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            //Setup File Filter
+            //TODO: CHANGE FILE TYPE TO VIDEO ONLY
+            fileDialog.Filter = "All Files (*.*)|*.*";
+            fileDialog.FilterIndex = 1;
+            fileDialog.Multiselect = false;
+
+            if(fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = Path.GetFullPath(fileDialog.FileName);
+                textBox2.Text = Path.GetFileName(fileDialog.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Please Select file want convert");
+            }
+            
+       
+        }
+
+
+        private void selectFolderLocation(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            textBox3.Text = dialog.SelectedPath.ToString();
+        }
+
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            flvToMP3(textBox1.Text);
+        }
+
+        static void flvToMP3(string FLVfilePath)
+        {
+            FLVFile test = new FLVFile(FLVfilePath);
+            test.ExtractStreams(true, false, false, null);
+            MessageBox.Show("it shouldddddddddddd");
+        }
+    }
+}
